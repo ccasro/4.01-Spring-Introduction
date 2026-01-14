afac3bdcccecdbc2342b422e8be7e5a1ea550ebd
ccasro <ccasro@gmail.com>
2026-01-14 12:20:24 +0100

    fix: changed String id by UUID as requested

--- src/main/java/cat/itacademy/s04/t01/userapi/model/User.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/model/User.java
@@ -2,8 +2,8 @@ package cat.itacademy.s04.t01.userapi.model;
 
 import java.util.UUID;
 
-public record User(String id, String name, String email) {
+public record User(UUID id, String name, String email) {
     public static User create(String name, String email) {
-        return new User(UUID.randomUUID().toString(), name, email);
+        return new User(UUID.randomUUID(), name, email);
     }
 }
