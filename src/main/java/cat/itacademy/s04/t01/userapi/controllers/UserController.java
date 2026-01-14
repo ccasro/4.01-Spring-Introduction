a4eec16282baf9c2c2ee2098f6dbd065f3377651
ccasro <ccasro@gmail.com>
2026-01-14 12:21:49 +0100

    feat: add endpoint to getUserById

--- src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -1,11 +1,13 @@
 package cat.itacademy.s04.t01.userapi.controllers;
 
 import cat.itacademy.s04.t01.userapi.dto.CreateUserRequest;
+import cat.itacademy.s04.t01.userapi.exceptions.UserNotFoundException;
 import cat.itacademy.s04.t01.userapi.model.User;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
@@ -23,4 +25,12 @@ public class UserController {
         users.add(newUser);
         return newUser;
     }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable UUID id) {
+        return users.stream().filter(u -> u.id().equals(id)).findFirst()
+                .orElseThrow(() -> new UserNotFoundException(id.toString()));
+    }
+
+
 }
